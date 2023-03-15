d097b594687ab8b7a50886fa4b41cbb935228347
José Ovando <42974480+Jos260400@users.noreply.github.com>
2023-03-15 11:32:06 -0600

    Avances

--- public/src/pages/SetAvatar.jsx
+++ public/src/pages/SetAvatar.jsx
@@ -1,7 +1,33 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import loader from "../assets/loader.gif";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import { setAvatarRoute } from "../utils/APIRoutes";
 export default function SetAvatar() {
+    const api = `https://api.multiavatar.com/4645646`;
+    const navigate = useNavigate();
 
-    return <div>SetAvatar</div>;
+    return (
+        <>
+    
+  <Container>
+    
+    
+  <div className="title-container">
+  <h1>Pick an avatar as a profile picture</h1>
+  </div>
+    
+    
+    </Container>
 
+  <ToastContainer />
+
+
+</>
+);
 }
+
+const Container = styled.div``;
